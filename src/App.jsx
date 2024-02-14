e562afb800c3f507634cc715f085e4c72db2feeb
Dumindu Patabandi <74417715+DuminduPatabandi@users.noreply.github.com>
2024-02-14 16:38:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Card from './components/Card'
+import Card from './Components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
